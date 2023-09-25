24ffb7811d8b27163a5dc76406150d850b5ed0d0
José Silva <jose@MacBook-Air-de-Jose.local>
2023-09-25 13:32:23 -0300

    Teste unitário alcis consumer

---
+++ index.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    try {
+        const browser = await puppeteer.launch({ headless: "new" });
+        const page = await browser.newPage();
+        await page.goto('https://www.noticiasautomotivas.com.br/toyota-gr-yaris-ganhara-mais-potencia-chegando-a-304-cavalos/', { waitUntil: 'domcontentloaded', timeout: 60000 }); // Aumentar o tempo limite para 60 segundos
+
+        // Capturar a página inteira como um PDF
+        await page.pdf({ path: 'pagina.pdf', format: 'A4' });
+
+        console.log('Página convertida para PDF com sucesso e salva como pagina.pdf');
+
+        await browser.close();
+    } catch (error) {
+        console.error('Erro ao converter a página para PDF:', error);
+    }
+})();
---
+++ index2.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    try {
+        const browser = await puppeteer.launch({ headless: "new" });
+        const page = await browser.newPage();
+        await page.goto('https://www.noticiasautomotivas.com.br/toyota-gr-yaris-ganhara-mais-potencia-chegando-a-304-cavalos/', { waitUntil: 'domcontentloaded', timeout: 60000 });
+
+        // Aguarde até que a página esteja completamente carregada
+        await page.waitForSelector('.single-post-content');
+
+        // Extraia o texto do conteúdo da página
+        const pageText = await page.evaluate(() => {
+            const contentElement = document.querySelector('.single-post-content');
+            if (contentElement) {
+                return contentElement.textContent;
+            }
+            return ''; // Retorna uma string vazia se o elemento não for encontrado
+        });
+
+        // Salvar o texto em um arquivo PDF
+        const fs = require('fs');
+        await page.pdf({ path: 'conteudo.pdf', format: 'A4' });
+
+        console.log('Texto extraído da página e salvo como conteudo.pdf');
+
+        await browser.close();
+    } catch (error) {
+        console.error('Erro ao extrair o texto da página:', error);
+    }
+})();
